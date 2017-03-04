4c18b7eda11678d2ffdb37d04b6ec02384b8f206
Alex Yovev <ayovev511@gmail.com>
2017-03-03 16:28:17 -0800

    Update rng.cpp

--- project4/rng.cpp
+++ project4/rng.cpp
@@ -1,42 +1,28 @@
 #include <cstdlib>
 #include <iostream>
-#include <ctime>
+#include <chrono>
 #include <random>
 
 using namespace std;
 
-/*
 int main()
 {
-   srand(time(0));
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 
-   cout << rand() % 1000000 << endl;
+    default_random_engine dre(seed); // [1,2147483646]
+    mt19937 mt(seed); // [0,4294967295]
 
-   cout << endl;
+    uniform_int_distribution<unsigned long int> dist(1, 1000000);
 
-   return EXIT_SUCCESS;
-}
-*/
+    for( int i = 0; i < 5; i++ )
+    {
+      cout << "Random value from distribution using default_random_engine: " << dist(dre) << endl << endl;
+    }
 
-int main()
-{
-    random_device seed; // [0,4294967295]
-    default_random_engine engine; // [1,2147483646]
-    mt19937 mt(seed()); // [0,4294967295]
-    uniform_int_distribution<int> dist(1, 10);
-
-    cout << "Minimum: " << seed.min() << endl
-         << "Maximum: " << seed.max() << endl << endl;
-
-    cout << "Minimum: " << engine.min() << endl
-         << "Maximum: " << engine.max() << endl << endl;
-
-    cout << "Minimum: " << mt.min() << endl
-         << "Maximum: " << mt.max() << endl << endl;
-
-    cout << "Random value from random_device: " << seed() % 1000000 << endl
-         << "Random value from default_random_engine: " << engine() % 1000000 << endl
-         << "Random value from mt19937: " << mt() % 1000000 << endl << endl;
+    for( int i = 0; i < 5; i++ )
+    {
+      cout << "Random value from distribution using mt19937: " << dist(mt) << endl << endl;
+    }
 
     return EXIT_SUCCESS;
 }
