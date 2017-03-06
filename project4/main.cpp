b906bd9daa0d97630d878c99bec549abc556e875
Alex Yovev <ayovev511@gmail.com>
2017-03-05 19:31:34 -0800

    added bubble sort and starting working on quicksort

--- project4/main.cpp
+++ project4/main.cpp
@@ -6,12 +6,17 @@
 #include <chrono>
 #include <random>
 #include <vector>
+#include <utility>
+
+using namespace std;
 
 int randomNumberGenerator();
 void delay();
 void generateFile( int numValues );
-
-using namespace std;
+void bubbleSort( vector<int>& vec );
+void quickSort( vector<int>& vecE, int firstE, int lastE );
+//void part( vector<int>& vec );
+int part( vector<int>& vec, int first, int last );
 
 int main()
 {
@@ -38,9 +43,12 @@ int main()
       vec3[ index ] = number;
    }
 
+   bubbleSort( vec1 );
+   quickSort( vec2, vec2.front(), vec2.back() );
+
    for( index = 0; index < numValues; index++ )
    {
-      cout << index + 1 << ". " << vec1[index] << endl;
+      cout << index + 1 << ". " << vec2[index] << endl;
    }
 
    cout << endl;
@@ -55,9 +63,9 @@ int randomNumberGenerator()
    default_random_engine dre(seed); // [1,2147483646]
    mt19937 mt(seed); // [0,4294967295]
 
-   uniform_int_distribution<int> dist(1, 1000000);
+   uniform_int_distribution<int> dist(0, 1000000);
 
-   return dist( dre );
+   return dist( mt );
 }
 
 void delay()
@@ -84,4 +92,85 @@ void generateFile( int numValues )
    }
 
    fout.close();
+}
+
+void bubbleSort( vector<int>& vec )
+{
+   bool sorted = false;
+   int count, index;
+
+   for( count = 1; ( count < vec.size() ) && sorted == false; count++ )
+   {
+      sorted = true;
+
+      for( index = 0; index < vec.size() - count; index++ )
+      {
+         if( vec[ index ] > vec[ index + 1 ] )
+         {
+            swap( vec[ index ], vec[ index + 1 ] );
+            sorted = false;
+         }
+      }
+   }
+}
+
+void quickSort( vector<int>& vecE, int firstE, int lastE )
+{
+   int middle;
+
+   if( firstE < lastE )
+   {
+      cout << "HOPEFULLY THIS WORKS" << endl;
+      middle = part( vecE, vecE.front(), vecE.back() );
+      cout << "WELL THIS WORKED" << endl;
+      quickSort( vecE, firstE, middle );
+      quickSort( vecE, middle + 1, lastE );
+   }
+}
+
+/*
+void part( vector<int>& vec )
+{
+   int pivot = vec[0];
+   int left, right;
+
+   for( left = 1, right = vec.size() - 1; left < right; )
+   {
+      if( vec[ left ] > pivot && vec[ right ] <= pivot )
+      {
+         swap( vec[ left ], vec[ right ] );
+      }
+      if( vec[ left ] <= pivot )
+      {
+         left++;
+      }
+      if( vec[ right ] > pivot )
+      {
+         right--;
+      }
+   }
+}
+*/
+
+int part( vector<int>& vec, int first, int last )
+{
+   int pivot = vec[ first ];
+
+   while( first != last )
+   {
+      if( vec[ first ] > vec[ last ] )
+      {
+         swap( vec[ first ], vec[ last ] );
+      }
+      if( pivot == vec[ first ] )
+      {
+         last--;
+      }
+      else
+      {
+         first++;
+      }
+   }
+
+   return first;
 }
