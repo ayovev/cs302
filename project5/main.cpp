4aaf1bb70795999f26a9fda841a9fc379b79c198
Alex Yovev <ayovev511@gmail.com>
2017-04-02 09:03:22 -0700

    continued testing queue and priority queue implementation, so far so good

--- project5/main.cpp
+++ project5/main.cpp
@@ -23,5 +23,24 @@ int main()
 
    PriorityQueue linee;
 
+   linee.push( 1, 10 );
+   cout << linee.getFront() << endl;
+   linee.push( 2, 3 );
+   cout << linee.getFront() << endl;
+   linee.push( 3, 5 );
+   cout << linee.getFront() << endl;
+   linee.push( 4, 20 );
+   cout << linee.getFront() << endl;
+   linee.push( 5, 10 );
+   cout << linee.getFront() << endl;
+   linee.push( 6, 1 );
+   cout << linee.getFront() << endl;
+   linee.push( 7, 2 );
+   cout << linee.getFront() << endl;
+   linee.push( 8, 99 );
+   cout << linee.getFront() << endl;
+
+   cout << linee << endl << endl;
+
    return EXIT_SUCCESS;
 }
