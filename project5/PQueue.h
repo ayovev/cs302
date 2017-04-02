23ffefcf67571e5a2960c8bcc608c81597d6aed1
Alex Yovev <ayovev511@gmail.com>
2017-04-02 09:01:05 -0700

    added prototype for overloading the insertion operator, included library and namespace

--- project5/PQueue.h
+++ project5/PQueue.h
@@ -1,13 +1,20 @@
 #ifndef PRIORITY_QUEUE_H
 #define PRIORITY_QUEUE_H
 
+#include <iostream>
+
+using namespace std;
+
+class PriorityQueue;
+
 struct PNode
 {
-   PNode( int item, int prio );
    PNode( int item, int prio, PNode* next );
    int priority;
    int data;
    PNode* link;
+
+   friend ostream& operator <<( ostream& out, const PriorityQueue& object ); // NOT SURE IF THIS IS NEEDED
 };
 
 class PriorityQueue
@@ -19,6 +26,8 @@ class PriorityQueue
       bool pop();
       int getFront();
       bool isEmpty();
+
+      friend ostream& operator <<( ostream& out, const PriorityQueue& object );
 
    private:
       PNode* front;
