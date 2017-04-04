6d0242c5acd59346c525195266e925572309ef47
Alex Yovev <ayovev511@gmail.com>
2017-04-03 18:40:12 -0700

    changed push priority logic

--- project5/PQueue.cpp
+++ project5/PQueue.cpp
@@ -35,7 +35,7 @@ bool PriorityQueue::push( const int item, const int priority )
 {
    PNode* temp1 = new PNode( item, priority, NULL );
 
-   if( front == NULL || temp1->priority > front->priority )
+   if( front == NULL || temp1->priority < front->priority )
    {
       temp1->link = front;
       front = temp1;
@@ -46,7 +46,7 @@ bool PriorityQueue::push( const int item, const int priority )
    {
       PNode* temp2 = front;
 
-      while( temp2->link != NULL && temp2->link->priority >= temp1->priority )
+      while( temp2->link != NULL && temp2->link->priority <= temp1->priority )
       {
          temp2 = temp2->link;
       }
