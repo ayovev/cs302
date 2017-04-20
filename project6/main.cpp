965e462350be4b62755fac51777ef78b32f0d711
Alex Yovev <ayovev511@gmail.com>
2017-04-20 10:01:11 -0700

    testing more bst functions

--- project6/main.cpp
+++ project6/main.cpp
@@ -23,7 +23,44 @@ int main()
    generateInputFile( 100 );
    cout << "INPUT FILE GENERATED" << endl; // IFTP
    
+   cout << "INITIAL BST HEIGHT: " << tree1.getHeight( tree1.root ) << endl; // IFTP
+   
+   tree1.add( 10 );
+   cout << "ADDING NODE WITH VALUE 10" << endl;
    cout << "BST HEIGHT: " << tree1.getHeight( tree1.root ) << endl;
+   cout << "NUMBER OF NODES: " << tree1.getNumberOfNodes( tree1.root ) << endl;
+   
+   tree1.add( 20 );
+   cout << "ADDING NODE WITH VALUE 20" << endl;
+   cout << "BST HEIGHT: " << tree1.getHeight( tree1.root ) << endl;
+   cout << "NUMBER OF NODES: " << tree1.getNumberOfNodes( tree1.root ) << endl;
+   
+   tree1.add( 5 );
+   cout << "ADDING NODE WITH VALUE 5" << endl;
+   cout << "BST HEIGHT: " << tree1.getHeight( tree1.root ) << endl;
+   cout << "NUMBER OF NODES: " << tree1.getNumberOfNodes( tree1.root ) << endl;
+   
+   tree1.add( 18 );
+   cout << "ADDING NODE WITH VALUE 18" << endl;
+   cout << "BST HEIGHT: " << tree1.getHeight( tree1.root ) << endl;
+   cout << "NUMBER OF NODES: " << tree1.getNumberOfNodes( tree1.root ) << endl;
+   
+   tree1.add( 6 );
+   cout << "ADDING NODE WITH VALUE 6" << endl;
+   cout << "BST HEIGHT: " << tree1.getHeight( tree1.root ) << endl;
+   cout << "NUMBER OF NODES: " << tree1.getNumberOfNodes( tree1.root ) << endl;
+   
+   //cout << "CLEARING BST..." << endl << endl;
+   //tree1.clear( tree1.root );
+   
+   // cout << "BST HEIGHT: " << tree1.getHeight( tree1.root ) << endl;
+   // cout << "NUMBER OF NODES: " << tree1.getNumberOfNodes( tree1.root ) << endl;
+   // cout << "ADDRESS OF ROOT NODE: " << &tree1.root << endl;
+   // cout << "ROOT NODE DATA: " << tree1.getRootData() << endl;
+   
+   cout << tree1.contains( tree1.root, 0 ) << endl;
+   
+   system("pause");
    
    return EXIT_SUCCESS;
 }
