94c608cfe1d842b8ced967bb2a82343abf361678
Alex Yovev <ayovev511@gmail.com>
2017-04-20 22:03:24 -0700

    program layout finished, just need to implement remove function and clean up output formatting

--- project6/main.cpp
+++ project6/main.cpp
@@ -12,57 +12,86 @@
 using namespace std;
 
 void generateInputFile( const unsigned int numValues );
+void loadBST( BinarySearchTree& tree );
+void isBstEmpty( const BinarySearchTree& tree );
 
 int main()
 {
    srand( time( NULL ) );
-   
+
    BinarySearchTree tree1;
-   cout << "BINARY SEARCH TREE CONSTRUCTED" << endl; // IFTP
+   BinarySearchTree tree2;
+   bool found = false;
+   int count = 0;
    
    generateInputFile( 100 );
-   cout << "INPUT FILE GENERATED" << endl; // IFTP
    
-   cout << "INITIAL BST HEIGHT: " << tree1.getHeight( tree1.root ) << endl; // IFTP
+   loadBST( tree1 );
    
-// IFTP - START
-   // tree1.add( # );
-   // cout << "ADDING NODE WITH VALUE #" << endl;
-   // cout << "BST HEIGHT: " << tree1.getHeight( tree1.root ) << endl;
-   // cout << "NUMBER OF NODES: " << tree1.getNumberOfNodes( tree1.root ) << endl;
-// IFTP - END
-
-   tree1.add( 60 );
-   tree1.add( 20 );
-   tree1.add( 70 );
-   tree1.add( 10 );
-   tree1.add( 40 );
-   tree1.add( 30 );
-   tree1.add( 50 );
-   
-   //cout << "CLEARING BST..." << endl << endl;
-   //tree1.clear( tree1.root );
-   
-   // cout << "BST HEIGHT: " << tree1.getHeight( tree1.root ) << endl;
-   // cout << "NUMBER OF NODES: " << tree1.getNumberOfNodes( tree1.root ) << endl;
-   // cout << "ADDRESS OF ROOT NODE: " << &tree1.root << endl;
-   // cout << "ROOT NODE DATA: " << tree1.getRootData() << endl;
-   
-   // cout << tree1.contains( tree1.root, 0 ) << endl;
-   
-   cout << "PREORDER TRAVERSE: ";
-   tree1.preorderTraverse( tree1.root );
-   cout << endl << endl;
-   
-   cout << "POSTORDER TRAVERSE: ";
-   tree1.postorderTraverse( tree1.root );
-   cout << endl << endl;
-   
-   cout << "INORDER TRAVERSE: ";
+   cout << endl << "Height of BST1: " 
+        << tree1.getHeight( tree1.root ) << endl;
+        
+   cout << endl << "Inorder traversal of BST1: ";
    tree1.inorderTraverse( tree1.root );
    cout << endl << endl;
    
-   system("pause");
+   generateInputFile( 10 );
+   
+   loadBST( tree2 );
+   
+   cout << endl << "Preorder traversal of BST2: ";
+   tree2.preorderTraverse( tree2.root );
+   cout << endl << endl;
+   
+   cout << "Inorder traversal of BST2: ";
+   tree2.inorderTraverse( tree2.root );
+   cout << endl << endl;
+   
+   cout << "Postorder traversal of BST2: ";
+   tree2.postorderTraverse( tree2.root );
+   cout << endl << endl;
+   
+   for( found = false, count = 0; count < 200; count++ )
+   {
+      if( tree2.contains( tree2.root, count ))
+      {
+         found = true;
+      }
+      if( found == true )
+      {
+         // tree1.remove( tree1.root, count );
+      }
+      found = false;
+   }
+   
+   cout << "Height of modified BST1: " 
+        << tree1.getHeight( tree1.root ) << endl << endl;
+        
+   cout << "Number of nodes in modified BST1: "
+        << tree1.getNumberOfNodes( tree1.root ) << endl << endl;
+        
+   cout << "Inorder traversal of modified BST1: ";
+   tree1.inorderTraverse( tree1.root );
+   cout << endl << endl;
+   
+   cout << "BST1 empty? ";
+   isBstEmpty( tree1 );
+   cout << endl;
+   
+   cout << "BST2 empty? ";
+   isBstEmpty( tree2 );
+   cout << endl;
+   
+   tree1.clear( tree1.root );
+   tree2.clear( tree2.root );
+   
+   cout << "BST1 empty? ";
+   isBstEmpty( tree1 );
+   cout << endl;
+   
+   cout << "BST2 empty? ";
+   isBstEmpty( tree2 );
+   cout << endl;
    
    return EXIT_SUCCESS;
 }
@@ -87,7 +116,7 @@ void generateInputFile( const unsigned int numValues )
    
    for( count = 0; count < numbers.size(); count++ )
    {
-      if( count == 99 )
+      if( count == numbers.size() - 1 )
       {
          fout << numbers.at( count );
       }
@@ -98,4 +127,34 @@ void generateInputFile( const unsigned int numValues )
    }
    
    fout.close();
+}
+
+void loadBST( BinarySearchTree& tree )
+{
+   ifstream fin;
+   int value;
+   
+   fin.clear();
+   fin.open( "input.txt" );
+   
+   while( fin.eof() == false )
+   {
+      fin >> value;
+      cout << "Adding value " << value << " to binary search tree." << endl;
+      tree.add( value );
+   }
+   
+   fin.close();
+}
+
+void isBstEmpty( const BinarySearchTree& tree )
+{
+   if( tree.isEmpty() == false )
+   {
+      cout << "False";
+   }
+   else if( tree.isEmpty() == true )
+   {
+      cout << "True";
+   }
 }
