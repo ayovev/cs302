8a8ce0ec1f24bef35506e1cdcac7195cfb00175a
Alex Yovev <ayovev511@gmail.com>
2017-04-21 20:09:43 -0700

    commented node functions

--- project6/binaryNode.cpp
+++ project6/binaryNode.cpp
@@ -1,3 +1,4 @@
+// LIBRARIES AND HEADER FILES
 #include "binaryNode.h"
 #include <cstdlib>
 #include <iostream>
@@ -5,8 +6,10 @@
 using std::cout;
 using std::endl;
 
+// FUNCTION IMPLEMENTATION
 BinaryNode::BinaryNode()
-{   
+{
+   // set default data and set child pointers to NULL
    data = -1;
    leftChild = NULL;
    rightChild = NULL;
@@ -14,6 +17,7 @@ BinaryNode::BinaryNode()
 
 BinaryNode::BinaryNode( const int item )
 {   
+   // set data to parameter and set child pointers to NULL
    data  = item;
    leftChild = NULL;
    rightChild = NULL;
@@ -22,6 +26,7 @@ BinaryNode::BinaryNode( const int item )
 BinaryNode::BinaryNode( const int item,
                         BinaryNode* left, BinaryNode* right )
 {
+   // set data and children pointers to specified parameters
    data = item;
    leftChild = left;
    rightChild = right;
@@ -29,22 +34,26 @@ BinaryNode::BinaryNode( const int item,
 
 BinaryNode::~BinaryNode()
 {
+   // set left and right children to NULL
    leftChild = NULL;
    rightChild = NULL;
 }
 
 void BinaryNode::setItem( const int item )
 {
+   // set data
    data = item;
 }
 
 int BinaryNode::getItem() const
 {
+   // get data
    return data;
 }
 
 bool BinaryNode::isLeaf() const
 {
+   // check if both children of node are NULL
    if( leftChild == NULL && rightChild == NULL )
    {
       return true;
