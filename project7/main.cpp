3a1e9d67ed82ca131a9c6759b3f9b7b2072db9a5
Alex Yovev <ayovev511@gmail.com>
2017-05-01 07:45:29 -0700

    beginning testing on reading in json file

--- project7/main.cpp
+++ project7/main.cpp
@@ -1,13 +1,23 @@
+// include libraries and header files
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include "overview.h"
+#include "suit.h"
 #include "player.h"
 
 using namespace std;
 
+// main function
 int main()
 {
-
+   ifstream fin;
+   
+   fin.clear();
+   fin.open( "PlayerStatistics.json" );
+   
+   fin.close();
+   
+   // return successful program execution
    return EXIT_SUCCESS;
 }
