21b931cd204b214c41e155f333d18fb8bd770a9a
Alex Yovev <ayovev511@gmail.com>
2017-05-02 16:17:38 -0700

    added more read functions for modularity, still testing

--- project7/main.cpp
+++ project7/main.cpp
@@ -3,14 +3,18 @@
 #include <iostream>
 #include <fstream>
 #include "overview.cpp"
-#include "suit.h"
+#include "suit.cpp"
 #include "player.cpp"
 
 using namespace std;
 
+const int NUM_SUITS = 3;
+
+void prime( ifstream& fin );
 void readPlayerName( ifstream& fin, Player& player );
 void readOverview( ifstream& fin, Player& player );
-void prime( ifstream& fin );
+void readSuits( ifstream& fin, Player& player );
+
 
 // main function
 int main()
@@ -26,11 +30,33 @@ int main()
    
    readPlayerName( fin, playerOne );
    readOverview( fin, playerOne );
+   readSuits( fin, playerOne );
+   
+   // playerOne.printName();
+   // playerOne.printOverview();
+   // playerOne.printAllInfo();
    
    fin.close();
 
    // return successful program execution
    return EXIT_SUCCESS;
+}
+
+void prime( ifstream& fin )
+{
+   char temp;
+   
+   fin >> temp;
+   
+   while( temp != ':' )
+   {
+      fin >> temp;
+      
+      // // IFTP
+      // cout << temp << endl;
+      // 
+      // system("pause");
+   }
 }
 
 void readPlayerName( ifstream& fin, Player& player )
@@ -79,19 +105,59 @@ void readOverview( ifstream& fin, Player& player )
    player.all.setTopScore( value );
 }
 
-void prime( ifstream& fin )
+void readSuits( ifstream& fin, Player& player )
 {
    char temp;
+   int value;
+   string type;
    
-   fin >> temp;
+   prime( fin );
    
-   while( temp != ':' )
+   for( int i = 0; i < NUM_SUITS; i++ )
    {
+      prime( fin );
       fin >> temp;
       
-      // // IFTP
-      // cout << temp << endl;
-      // 
-      // system("pause");
+      getline( fin, type, '\"');
+      
+      player.suitType[ i ].setType( type );
+      
+      prime( fin );
+      fin >> value;
+      player.suitType[ i ].setGamesWon( value );    
+      
+      prime( fin );
+      fin >> value;
+      player.suitType[ i ].setWinRate( value );
+      
+      prime( fin );
+      fin >> value;
+      player.suitType[ i ].setGamesPlayed( value );
+      
+      prime( fin );
+      fin >> value;
+      player.suitType[ i ].setFastestWin( value );
+      
+      prime( fin );
+      fin >> value;
+      player.suitType[ i ].setFewestMoves( value );
+      
+      prime( fin );
+      fin >> value;
+      player.suitType[ i ].setTopScore( value );
+   }
+   
+   // // IFTP
+   // for( int j = 0; j < NUM_SUITS; j++ )
+   // {
+   //    cout << player.suitType[ j ].getType() << endl
+   //         << player.suitType[ j ].getGamesWon() << endl
+   //         << player.suitType[ j ].getWinRate() << endl
+   //         << player.suitType[ j ].getGamesPlayed() << endl
+   //         << player.suitType[ j ].getFastestWin() << endl
+   //         << player.suitType[ j ].getFewestMoves() << endl
+   //         << player.suitType[ j ].getTopScore() << endl << endl;
+   //         
+   //    system("pause");
    }
 }
